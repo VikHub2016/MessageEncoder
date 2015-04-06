109c4413c8770a64902c686beeb17e60e5e9f12c
VikHub2016 <vikram.r.wwhs@gmail.com>
2015-04-06 14:15:35 -0500

    Finished write (I think)

--- src/Encoder.java
+++ src/Encoder.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class Encoder {
 	private File f;
 	private ArrayList<String> letters = new ArrayList<String>();
+	private String encodString;
 
 	public Encoder(String filename)
 	{
@@ -15,25 +16,29 @@ public class Encoder {
 		f = file;
 	}
 	
-	public void write(String plaintext)
+	public void write(String plaintext) throws IOException
 	{
 		for(int j = 0;j<plaintext.length()-1;j++)
-			{
-				letters.add(plaintext.substring(j, j+1));
-			}
+		{
+			letters.add(plaintext.substring(j, j+1));
+		}
+		//encoding letters in message provided
 		for(int j = 0;j<letters.size();j++)
 		{
 			char c = letters.get(j).charAt(0);
 			int encodChar = (int) c;
 			encodChar = encodChar+17;
-			
+			encodString = ":" + encodChar;
 		}
 		
+		FileWriter writer = new FileWriter(f,false);		
+		writer.write(encodString);
 
 	}
 	
 	public List<String> read()
 	{
 		return null;
+		
 	}
 }
