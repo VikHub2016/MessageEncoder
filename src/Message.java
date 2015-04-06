1287a8263c9aa874339299c330dd5af4ee8b09c8
LegitMikeyG <michael.goolsbey@gmail.com>
2015-04-06 13:01:07 -0500

    Added some stuff to the Message class

--- src/Message.java
+++ src/Message.java
@@ -1,4 +1,14 @@
+import java.awt.List;
+import java.util.ArrayList;
 
-public class Message {
 
+public class Message 
+{
+	private ArrayList<String> messages = new ArrayList<String>();
+	private String Username;
+	
+	
+	
+	
+	
 }
