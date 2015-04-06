86420bbaa214b353b264963c7e4c541db0e93da6
LegitMikeyG <michael.goolsbey@gmail.com>
2015-04-06 14:31:51 -0500

    Started getMessages

--- src/BackEnd.java
+++ src/BackEnd.java
@@ -1,5 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class BackEnd 
 {
-
+	
+	
+	
+	public List<String> getMessages(String username)
+	{
+		
+		List<String> list = new ArrayList<String>();
+		Encoder x = new Encoder(username + ".txt");
+		List<String> userMessages = new ArrayList<String>();
+		userMessages = x.read();
+		for(int i = 0; i < userMessages.size(); i++)
+		{
+			
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
