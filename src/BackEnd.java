227cf0cb17f842d426b261200415f0d122ccfa74
LegitMikeyG <michael.goolsbey@gmail.com>
2015-04-08 12:35:56 -0500

    Finished getMessages, started addMessage

--- src/BackEnd.java
+++ src/BackEnd.java
@@ -11,15 +11,30 @@ public class BackEnd
 	{
 		
 		List<String> list = new ArrayList<String>();
-		Encoder x = new Encoder(username + ".txt");
+		Encoder x = new Encoder("file.txt");
 		List<String> userMessages = new ArrayList<String>();
 		userMessages = x.read();
-		for(int i = 0; i < userMessages.size(); i++)
+		for(int i = 0; i < userMessages.size(); i = i + 2)
 		{
-			
+			if(userMessages.get(i).equals(username))
+			{
+				list.add(userMessages.get(i));
+			}
 		}
 		
+		if(!(list==null))
+		{
+			return list;
+		}
+		return null;
+	
 		
+	}
+	
+	public void addMessage(String username, String msg)
+	{
+		String encoded;
+		Encoder x = new Encoder("file.txt");
 	}
 	
 	
